f3c7e5891ad826353aed24d10aa6f3e3dc2c5a51
WaceraMercyThird <waceraaamercy@gmail.com>
2022-04-30 16:49:27 +0300

    valiable

---
+++ main.py
@@ -0,0 +1,40 @@
+
+
+# Sale prediction using valiables
+
+from tokenize import Double
+
+
+profit_percentage = 0.23
+
+
+total_sale = float(input("Enter the amount of total sale: "))
+
+profit = total_sale * profit_percentage
+
+print("Your total profit is:", profit)
+
+percent_of_the_amount = 23000.00
+
+total_amount = float(input("Enter deposit: "))
+
+balance = total_amount * percent_of_the_amount 
+
+print("Your amount blance is: ", balance)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
